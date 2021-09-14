18ba7b42e08b4f051c7fda9b5e0a971a85bf972c
HeyItsBran <90723744+HeyItsBran@users.noreply.github.com>
2021-09-14 16:29:34 -0400

    Added files via upload
    Added Exercise2.0.py to Week 2 folder

---
+++ Week 2/Exercise2.0.py
@@ -0,0 +1,11 @@
+import time
+name = str("Branden Dillon")
+date = str("September 14th, 2021")
+question = str("At what size of a project should you start using version management software? Why? Minimum 150 words.")
+response = str("I think that you should use version management software no matter how many people are on your team. Using version management software is easy, convenient, usually free and can save you SO MUCH time and pain. If you lose a chunk of code somehow, roll back to a version that still had that crucial component. Even if you lose your entire project, having a version saved somewhere out there is way better than having to come up with an excuse as to why your homework is going to be late again. Having the ability to roll-back changes to your projects if something goes wrong, a built in log of who made changes and when, backups in case of catastrophe; it would be a disservice to yourself, your team mates and your projects to actively choose not to use a version management software. Even if you're just working by yourself, using version management software is a great idea.")
+print(name)
+print(date)
+print(question)
+time.sleep(2.5)
+print(response)
+time.sleep(20)
